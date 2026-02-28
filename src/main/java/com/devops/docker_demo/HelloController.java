e4e493a62a9aed2ef5a0d94009f98e47c09c390c
Mr. PNS <pratiksatalkar6@gmail.com>
2026-03-01 01:36:39 +0530

    Initial Spring Boot + Docker setup 2

--- src/main/java/com/devops/docker_demo/HelloController.java
+++ src/main/java/com/devops/docker_demo/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+//test line
     @GetMapping("/")
     public String hello() {
         return "Hello DevOps from Docker !!";
